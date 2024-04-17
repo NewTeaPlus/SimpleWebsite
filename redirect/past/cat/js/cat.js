ac0c7229bc4e48691e94780d20ad985d22f30cc4
NewTeaPlus <NewTeaPlus@yahoo.com>
2024-04-16 17:05:30 -0700

    Sound Effects working! Now!

--- redirect/past/cat/js/cat.js
+++ redirect/past/cat/js/cat.js
@@ -7,27 +7,29 @@ cat.addEventListener("click", life)
 
 function life(){
     if(hp >= 4000){
-        let swipe = new Audio("../audio/Swipe.mp3")
-        swipe.play()
+        var swipe = new Audio("audio/LightSwipe.mp3");
+        swipe.play();
         hp -= 250
         lifelog.innerHTML=`CAT SWIPES LIGHTLY!
         <br> You lose 250 HP </br>
         <br> HP: ${hp}/5000 </br>`
     } else if (hp >= 1000) {
-        let swipe = new Audio()
+        var swipe = new Audio("audio/Swipe.mp3");
+        swipe.play();
         hp -= 1250
         lifelog.innerHTML=`CAT SWIPES!
         <br> You lose 1250 HP </br>
         <br> HP: ${hp}/5000 </br>`
     }else if(hp >= 0 ){
-        let swipe = new Audio()
+        var swipe = new Audio("audio/HeavyFastSwipe.mp3");
+        swipe.play();
         hp -= 9999999
         lifelog.innerHTML=`CAT SWIPES WITH A MIGHTY SWIPE!
         <br>CRITICAL HIT!!!!</br>
         <br> You lose 9,999,999 HP </br>
         <br> HP: ${hp}/5000 </br>`
     }else{
-    alert(`You fainted, Cat Drags you back to main page`)
+    alert(`You fainted, Cat Drags you back to main page`);
     //add a url to go back to
     window.location.href="../../../index.html"
     }
